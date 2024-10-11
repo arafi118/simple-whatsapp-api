b94dc32f392da15839e0f48c43ef50dcd5dc29d1
arafi118 <arp46301@gmail.com>
2024-10-11 09:51:24 +0700

    PORT

--- app.js
+++ app.js
@@ -10,26 +10,13 @@ require('./config/model')(fastify);
 require('./config/config')(fastify);
 require('./config/routes')(fastify);
 
-if (typeof (PhusionPassenger) != 'undefined') {
-  PhusionPassenger.configure({
-    autoInstall: true
-  });
-}
-
 const port = process.env.APP_PORT || 3000;
 const start = async () => {
   var server = process.env.APP_HOST;
 
   try {
-    if (typeof (PhusionPassenger) !== 'undefined') {
-      fastify.listen({
-        path: 'passenger',
-        host: '127.0.0.1'
-      })
-    } else {
-      fastify.listen(port)
-      server += ':' + port;
-    }
+    fastify.listen(port)
+    server += ':' + port;
 
     fastify.log.info(`Server running at ${server}`);
   } catch (err) {
