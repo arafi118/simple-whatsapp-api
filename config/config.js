7c9ebfe5cc5605978dbfe903f1a6de9fce27f863
arafi118 <arp46301@gmail.com>
2024-10-11 10:32:33 +0700

    CORS

--- config/config.js
+++ config/config.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+
 const path = require('path');
 
+const Cors = process.env.CORS_ORIGINS.replace(/"/g, '');
 module.exports = (fastify) => {
   fastify.register(require('@fastify/static'), {
     root: path.join(__dirname, '../resources/assets'),
     prefix: '/assets/',
+  });
+
+  fastify.register(require('@fastify/cors'), {
+    origin: (origin, callback) => {
+      if (!origin ||
+        origin === 'http://localhost' ||
+        origin === 'http://127.0.0.1' ||
+        new RegExp(`^https://([a-z0-9-]+\\.)?${allowedOrigin.replace(/^https?:\/\//, '')}$`).test(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
   });
 
   fastify.register(require('@fastify/view'), {
--- config/socket.js
+++ config/socket.js
@@ -1,9 +1,27 @@
+require('dotenv').config();
+
 const {
   Server
 } = require('socket.io');
 
+const Cors = process.env.CORS_ORIGINS.replace(/"/g, '');
 module.exports = (fastify) => {
-  const io = new Server(fastify.server);
+  const io = new Server(fastify.server, {
+    cors: {
+      origin: (origin, callback) => {
+        if (!origin ||
+          origin === 'http://localhost' ||
+          origin === 'http://127.0.0.1' ||
+          new RegExp(`^https://([a-z0-9-]+\\.)?${allowedOrigin.replace(/^https?:\/\//, '')}$`).test(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      credentials: true
+    }
+  });
 
   io.on('connection', (socket) => {
     console.log('New client connected');
