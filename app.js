60ca2ec3c8daff09e487242001428d758887e4da
arafi118 <arp46301@gmail.com>
2024-10-11 08:59:32 +0700

    Passenger

--- app.js
+++ app.js
@@ -18,16 +18,20 @@ if (typeof (PhusionPassenger) != 'undefined') {
 
 const port = process.env.APP_PORT || 3000;
 const start = async () => {
+  var server = process.env.APP_HOST;
+
   try {
     if (typeof (PhusionPassenger) !== 'undefined') {
       fastify.listen({
         path: 'passenger',
-        host: '127.0.0.1'
+        host: process.env.APP_HOST
       })
     } else {
       fastify.listen(port)
+      server += ':' + port;
     }
-    fastify.log.info(`Server running at http://localhost:${port}`);
+
+    fastify.log.info(`Server running at ${server}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
