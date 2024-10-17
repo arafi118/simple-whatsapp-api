75ff971d4b68db49129624228a1dbd9a9e7fc1cd
arafi118 <arp46301@gmail.com>
2024-10-17 07:58:17 +0700

    Cors

--- config/config.js
+++ config/config.js
@@ -13,6 +13,8 @@ module.exports = (fastify) => {
     origin: (origin, cb) => {
       if (!origin) return cb(null, true);
 
+      console.log(Cors, origin);
+
       const regexList = Cors.map(domain => {
         return new RegExp(`^https?:\/\/(.*\\.)?${domain.replace(/\./g, '\\.')}$`);
       });
